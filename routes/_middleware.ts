06e2a7e6f107eafc7eb788b2dc48acb7f857ccda
Garrison <gterrusa743@gmail.com>
2022-10-15 10:21:45 -0700

    update db connection

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,8 +1,16 @@
 import { remultFresh } from "remult/remult-fresh";
 import { Blog } from "../model/blog.ts";
+import { createPostgresConnection } from "https://deno.land/x/remult@v0.16.3-exp.3/postgres.ts";
 
 export const remultServer = remultFresh({
   entities: [Blog],
+  dataProvider: async () => {
+    const dbUrl = Deno.env.get("DATABASE_URL");
+    if (dbUrl) {
+      return createPostgresConnection({ connectionString: dbUrl });
+    }
+    return await undefined;
+  },
 }, Response);
 
 export const handler = remultServer.handle;
